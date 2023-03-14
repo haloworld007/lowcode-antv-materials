2112b24ea7cbd5e6b4e857416605ca3d2542202d
haloworld007 <891567977@qq.com>
2023-03-14 19:00:44 +0800

    feat: 完善line组件属性

--- src/components/line/index.tsx
+++ src/components/line/index.tsx
@@ -1,12 +1,24 @@
-import React, { createElement } from 'react';
+import React, { createElement, useEffect, useState } from 'react';
 import { Line as AntVLine, LineConfig } from '@ant-design/plots';
 
 export interface LineProps {
-  data: LineConfig['data'];
+  data?: LineConfig['data'];
+  dataUrl?: string;
+  xField: string;
+  yField: string;
 }
 
-const Line: React.FC<LineProps> = () => {
-  return <AntVLine data={[]} />;
+const Line: React.FC<LineProps> = ({ xField, yField, data: originData = [], dataUrl }) => {
+  const [data, setData] = useState(originData);
+
+  useEffect(() => {
+    if (dataUrl) {
+      // 如果有http接口，则请求写到data
+      setData([]);
+    }
+  }, [dataUrl]);
+
+  return <AntVLine data={data} xField={xField} yField={yField} />;
 };
 
 Line.displayName = 'Line';
